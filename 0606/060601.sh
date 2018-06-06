86df783b8813c846da60455a42e6de0717018870
n18015 <n18015@std.it-college.ac.jp>
2018-06-06 03:52:56 +0000

    kadai0606

---
+++ 0606/060601.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/vagrant
+_IFS=$IFS
+IFS=$'\n'
+
+for file in $(find . -type f)
+do 
+  size=$(ls -l $file | awk '{print $5}')
+if [ $size -eq 0 ]; then
+   echo "$file"," $size"
+fi
+done 
+
+fi
+done
